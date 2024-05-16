d7fcc287b4401378573c1e495f2b337b6695c7e4
muzappar <muzappar.ce@foxmail.com>
2024-05-16 14:08:25 +0800

    feature: decode byte array directly

--- VLQ.cs
+++ VLQ.cs
@@ -9,7 +9,7 @@ namespace VariableLengthQuantity;
 public static class VLQ
 {
     /**
-     * <summary>Read a VLQ from stream</summary>
+     * <summary>Read a VLQ from stream.</summary>
      * <param name="stream">Target stream to read from</param>
      * <returns>The first 7-bit encoded number read from the string</returns>
      */
@@ -42,13 +42,13 @@ public static class VLQ
         return result;
     }
     /**
-     * <summary>Write a variable-length number to the stream</summary>
+     * <summary>Write a variable-length number to the stream.</summary>
      * <param name="value">the value to encode and write to the stream</param>
      * <param name="stream">the stream to write to</param>
      */
     public static void Write(BigInteger value, Stream stream) => stream.Write(Encode(value));
     /**
-     * <summary>Encode a value as variable-length with each group of length 7</summary>
+     * <summary>Encode a value as variable-length with each group of length 7.</summary>
      * <param name="value">the value to encode</param>
      * <returns>the encoded byte sequence in big endian order</returns>
      */
@@ -65,23 +65,35 @@ public static class VLQ
         } while (value > 0);
         return [.. bytes];
     }
+    /**
+     * <summary>Decode a byte sequence to a number.</summary>
+     * <param name="bytes">Target byte sequence to encode</param>
+     * <returns>The decoded number</returns>
+     */
+    public static BigInteger Decode(byte[] bytes) => Read(new MemoryStream(bytes));
 
     /**
-     * <summary>Read a VLQ from stream</summary>
+     * <summary>Read a VLQ from stream.</summary>
      * <param name="stream">Target stream to read from</param>
      * <returns>The first 7-bit encoded number read from the string</returns>
      */
     public static T Read<T>(Stream stream) where T : INumberBase<T> => T.CreateChecked(Read(stream));
     /**
-     * <summary>Write a variable-length number to the stream</summary>
+     * <summary>Write a variable-length number to the stream.</summary>
      * <param name="value">the value to encode and write to the stream</param>
      * <param name="stream">the stream to write to</param>
      */
-    public static void Write<T>(T value, Stream stream) where T : INumber<T> => stream.Write(Encode(value));
+    public static void Write<T>(T value, Stream stream) where T : INumberBase<T> => stream.Write(Encode(value));
     /**
-     * <summary>Encode a value as variable-length with each group of length 7</summary>
+     * <summary>Encode a value as variable-length with each group of length 7.</summary>
      * <param name="value">the value to encode</param>
      * <returns>the encoded byte sequence in big endian order</returns>
      */
     public static byte[] Encode<T>(T value) where T : INumberBase<T> => Encode(BigInteger.CreateChecked(value));
+    /**
+     * <summary>Decode a byte sequence to a number.</summary>
+     * <param name="bytes">Target byte sequence to encode</param>
+     * <returns>The decoded number</returns>
+     */
+    public static T Decode<T>(byte[] bytes) where T : INumberBase<T> => T.CreateChecked(Decode(bytes));
 }
